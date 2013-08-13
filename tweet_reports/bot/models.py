2771067557e29851bef3f62f8418a9ca0f7b05a4
anvisha <anvisha@gmail.com>
2013-08-13 01:01:17 -0700

    updated models a little bit

--- tweet_reports/bot/models.py
+++ tweet_reports/bot/models.py
@@ -7,16 +7,16 @@ class Bot(models.Model):
 	twitter_handle = models.CharField(max_length=30)
 	oauth_token  = models.CharField(max_length=200)
 	oauth_secret = models.CharField(max_length=200)
+	#need to keep list of followers so bot knows who to follow back
 	def __unicode__(self):
 		return self.twitter_handle
 
 #The parsed version of an incoming tweet
 class Update(models.Model):
+	bot = models.ForeignKey(Bot) #which Bot the Update is associated with
 	tweet_text = models.CharField(max_length=1000)
 
 	#handle the tweet came from
 	tweet_origin = models.CharField(max_length=30) 
 	tweet_geocode = models.CharField(max_length=200)
 	#tweet_image = models.ImageField(upload_to=None)
-
-
